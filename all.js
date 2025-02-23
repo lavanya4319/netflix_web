c7fbfd459cc91578fdc01c311e66272ac1b364fc
lavanya4319 <padalalvn@gmail.com>
2025-02-23 13:06:17 +0530

    secondcommit

--- all.js
+++ all.js
@@ -1,1 +1,53 @@
-all.js
+let movies=[
+{
+   name:"",
+   poster:"",
+   rating:
+
+},
+{
+    name:"",
+   poster:"",
+   rating:
+
+
+
+},
+{
+
+    name:"",
+   poster:"",
+   rating:
+
+
+},
+{
+    name:"",
+   poster:"",
+   rating:
+
+
+
+
+},
+{
+    name:"",
+   poster:"",
+   rating:
+
+
+
+
+}
+
+
+];
+
+
+
+function selmovies()
+{
+  let movies=document.GetElementById('search').value;
+  console.log(movies);
+}
+    
